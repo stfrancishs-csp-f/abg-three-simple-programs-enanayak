f110aff56a481cf71625fdf68137ea0ac774fc3a
Ena Nayak <20nayak@stfrancishs.org>
2018-08-31 19:58:21 +0000

    init

---
+++ ABG2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+        printf("I am learning the %c programming language\n", 'C');
+        printf("I have just completed Chapter %d\n", 2);
+        printf("I am %1.f percent ready to move on", 99.9);
+        printf("t0o the next chapter!\n");
+        return 0;
+}
